a23fff0918a58d9efa4b4bc1cc025d4c6b6ddf28
Shun Kakinoki <shunkakinoki@gmail.com>
2020-04-23 17:39:21 +0900

    refactor: 💡 to correct bucket

--- infrastructure/modules/storage/reg.tf
+++ infrastructure/modules/storage/reg.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "reg" {
   count    = var.environment == "master" ? 1 : 0
-  name     = "sentrei-${var.environment}-reg"
+  name     = "sentrei-${var.environment}-reg-web"
   location = "US"
 }
