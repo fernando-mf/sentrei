7f1915cfa9bd771c47f6788b93dad4883c04bbcc
Shun Kakinoki <shunkakinoki@users.noreply.github.com>
2020-04-27 17:22:53 +0000

    [sentrei-auto-fix]

--- packages/web/src/pages/about.tsx
+++ packages/web/src/pages/about.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
-import {Copyright, ProTip} from "@sentrei/ui"
+import {Copyright, ProTip} from "@sentrei/ui";
 
 export default function About(): JSX.Element {
   return (
