175338a0a260310db9b05bb380408cd171646c0c
Shun Kakinoki <shunkakinoki@gmail.com>
2020-04-30 14:27:16 +0900

    feat: 🎸 add tm modules

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -2,6 +2,7 @@ const withPlugins = require("next-compose-plugins");
 const withSass = require("@zeit/next-sass");
 const withBundleAnalyzer = require("@next/bundle-analyzer");
 const nextRuntimeDotenv = require("next-runtime-dotenv");
+const withTM = require("next-transpile-modules")(["@sentrei/ui"]);
 
 const withConfig = nextRuntimeDotenv({
   public: ["API_URL", "API_KEY"],
@@ -15,5 +16,5 @@ const nextConfig = {
 };
 
 module.exports = withConfig(
-  withPlugins([[withSass], [withBundleAnalyzer]], nextConfig),
+  withPlugins([[withBundleAnalyzer], [withSass], [withTM]], nextConfig),
 );
