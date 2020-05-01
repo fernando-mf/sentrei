977cbb96a82454859bfc503292cc5555c3618eea
Shun Kakinoki <shunkakinoki@users.noreply.github.com>
2020-05-01 15:11:22 +0000

    [sentrei-auto-fix]

--- packages/web/next-env.d.ts
+++ packages/web/next-env.d.ts
@@ -1,3 +1,3 @@
-/// <reference types="next" />
-/// <reference types="next/types/global" />
-/// <reference types="next/dist/next-server/lib/utils" />
+// / <reference types="next" />
+// / <reference types="next/types/global" />
+// / <reference types="next/dist/next-server/lib/utils" />
