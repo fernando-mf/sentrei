f3adbce4e12d03d32fc364587219935707cac432
Shun Kakinoki <shunkakinoki@gmail.com>
2020-05-03 13:06:54 +0900

    style: 💄 add count index

--- infrastructure/modules/storage/reg.tf
+++ infrastructure/modules/storage/reg.tf
@@ -16,7 +16,7 @@ resource "google_storage_bucket" "reg" {
 
 resource "google_storage_bucket_iam_member" "reg" {
   count  = var.environment == "master" ? 1 : 0
-  bucket = google_storage_bucket.reg.name
+  bucket = google_storage_bucket.reg[count.index].name
   role   = "roles/storage.objectViewer"
   member = "allUsers"
 }
