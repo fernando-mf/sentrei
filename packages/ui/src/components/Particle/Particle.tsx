3ea23b7145342c5a060c532e2e5c89bbf0f3aa36
Shun Kakinoki <shunkakinoki@gmail.com>
2020-05-10 09:43:38 +0900

    feat: 🎸 add particle component

---
+++ packages/ui/src/components/Particle/Particle.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import Particles from "react-particles-js";
+
+const ParticlesComponent = (): JSX.Element => {
+  return (
+    <>
+      <Particles
+        params={{
+          particles: {
+            number: {
+              value: 160,
+              density: {
+                enable: false,
+              },
+            },
+            size: {
+              value: 3,
+              random: true,
+              anim: {
+                speed: 4,
+                size_min: 0.3,
+              },
+            },
+            line_linked: {
+              enable: false,
+            },
+            move: {
+              random: true,
+              speed: 1,
+            },
+          },
+          interactivity: {
+            modes: {
+              bubble: {
+                distance: 250,
+                duration: 2,
+                size: 0,
+                opacity: 0,
+              },
+              repulse: {
+                distance: 400,
+                duration: 4,
+              },
+            },
+          },
+        }}
+      />
+    </>
+  );
+};
+
+export default ParticlesComponent;
