53554ebd361e29531363cb2a5b0e2a5a28f25a31
Shun Kakinoki <shunkakinoki@gmail.com>
2020-05-13 20:03:58 +0900

    feat: add proper storybook webpack config

--- packages/ui/.storybook/webpack.config.js
+++ packages/ui/.storybook/webpack.config.js
@@ -10,26 +10,23 @@ module.exports = ({config}) => {
     ...config.resolve.alias,
     ...aliases,
   };
-  config.module.rules.push({
-    test: /\.(ts|tsx)$/,
-    use: [
-      {
-        loader: require.resolve("babel-loader"),
-        options: {
-          presets: [
-            [
-              "@babel/preset-env",
-              {
-                useBuiltIns: "usage",
-                corejs: 3,
-              },
-            ],
-          ],
+  config.module.rules = [
+    {
+      test: /\.(ts|tsx)$/,
+      include: path.resolve(__dirname, "../src"),
+      use: [
+        require.resolve("ts-loader"),
+        {
+          loader: require.resolve("react-docgen-typescript-loader"),
+          options: {
+            tsconfigPath: path.resolve(__dirname, "../tsconfig.json"),
+          },
         },
-      },
-    ],
-  });
+      ],
+    },
+  ];
 
   config.resolve.extensions.push(".ts", ".tsx");
+
   return config;
 };
