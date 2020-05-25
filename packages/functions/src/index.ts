32d5b8da7569cb01a35e7c89d598641ab9ef7076
Shun Kakinoki <shunkakinoki@gmail.com>
2020-05-25 15:27:43 +0900

    feat: complete initial function

--- packages/functions/src/index.ts
+++ packages/functions/src/index.ts
@@ -1,7 +1,29 @@
+/* eslint-disable global-require */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import * as admin from "firebase-admin";
 import * as functions from "firebase-functions";
 
-const helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!\n\n");
+let serviceAccount: any;
+let storageBucket = "";
+const environment = process.env.GCLOUD_PROJECT;
+if (environment === "sentrei-alpha") {
+  serviceAccount = require("../key/functions-admin-alpha.json");
+  storageBucket = "sentrei-alpha.appspot.com";
+} else if (environment === "sentrei-beta") {
+  serviceAccount = require("../key/functions-admin-beta.json");
+  storageBucket = "sentrei-beta.appspot.com";
+} else if (environment === "sentrei-master") {
+  serviceAccount = require("../key/functions-admin-master.json");
+  storageBucket = "sentrei-master.appspot.com";
+}
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  storageBucket,
 });
 
-export default helloWorld;
+const getEnvironment = functions.https.onCall((data, context) => {
+  return {environment};
+});
+
+export default getEnvironment;
