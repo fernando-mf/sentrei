f64f61c3ecdaceddc223a4736c42a486fe2650e7
Shun Kakinoki <shunkakinoki@gmail.com>
2020-05-27 18:39:46 +0900

    chore: comment out cypress

--- packages/web/cypress/plugins/index.js
+++ packages/web/cypress/plugins/index.js
@@ -4,5 +4,5 @@ module.exports = (on, config) => {
   require("@cypress/code-coverage/task")(on, config);
   on("file:preprocessor", cypressTypeScriptPreprocessor);
   // TODO: Fix cypress typescript bug #1916
-  on("file:preprocessor", require("@cypress/code-coverage/use-babelrc"));
+  // on("file:preprocessor", require("@cypress/code-coverage/use-babelrc"));
 };
