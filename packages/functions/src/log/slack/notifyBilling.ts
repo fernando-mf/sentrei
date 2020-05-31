2114157453cfb2b2ebe8aedf48dff46160e79587
Shun Kakinoki <shunkakinoki@gmail.com>
2020-05-31 19:47:15 +0900

    fix: 🐛 json parse pubsub

--- packages/functions/src/log/slack/notifyBilling.ts
+++ packages/functions/src/log/slack/notifyBilling.ts
@@ -5,10 +5,15 @@ const config = functions.config().env;
 
 const webhook = new IncomingWebhook(config.slack.url);
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const eventToBilling = (data: any): JSON => {
+  return JSON.parse(Buffer.from(data, "base64").toString());
+};
+
 const notifyBilling = functions.pubsub
   .topic(`sentrei-${config.environment}-billing`)
   .onPublish(event => {
-    const pubSubMessage = event.data;
+    const pubSubMessage = eventToBilling(event.data);
     (async (): Promise<void> => {
       await webhook.send({
         text: `${pubSubMessage}`,
