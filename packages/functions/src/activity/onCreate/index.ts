65176b92b0ab21c94a41e094f240f52eb254f189
Shun Kakinoki <shunkakinoki@gmail.com>
2020-06-08 10:16:42 +0900

    fix: 🐛 add activity room

--- packages/functions/src/activity/onCreate/index.ts
+++ packages/functions/src/activity/onCreate/index.ts
@@ -1,2 +1,5 @@
 /* eslint-disable import/prefer-default-export */
+export {default as actionCategorySet} from "./actionCategorySet";
+export {default as activityBatchSet} from "./activityBatchSet";
+export {default as scoreBatchUpdate} from "./scoreBatchUpdate";
 export {default as userNotificationBatchCreate} from "./userNotificationBatchCreate";
--- packages/functions/src/index.ts
+++ packages/functions/src/index.ts
@@ -10,6 +10,7 @@ import * as log from "./log";
 import * as members from "./members";
 import * as notifications from "./notifications";
 import * as profiles from "./profiles";
+import * as rooms from "./rooms";
 import * as spaces from "./spaces";
 import * as usernames from "./usernames";
 import * as users from "./users";
