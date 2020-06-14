4b7c2bf8ee1e06ccb208c5eba185fe9abc36ca9d
Shun Kakinoki <shunkakinoki@gmail.com>
2020-06-15 04:47:03 +0900

    fix: 🐛 perfect react typical (typist)

--- packages/ui/src/components/Typical/Typical.tsx
+++ packages/ui/src/components/Typical/Typical.tsx
@@ -1,13 +1,32 @@
 import React from "react";
 import {useInView} from "react-intersection-observer";
 import {useSpring, animated} from "react-spring";
-
 import Typist from "react-typist";
+import styled from "styled-components";
 
 import Props from "@sentrei/common/interfaces/Typical";
 
 import TypicalStyles from "./TypicalStyles";
 
+const StyledTypist = styled(Typist)`
+  .Cursor {
+    animation-name: blinker;
+    animation-duration: 1.2s;
+    animation-timing-function: linear;
+    animation-iteration-count: infinite;
+  }
+  @keyframes blinker {
+    0% {
+      opacity: 1;
+    }
+    50% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 1;
+    }
+  }
+`;
 export default function CustomTypical({
   typicalOne,
   typicalTwo,
@@ -29,7 +48,7 @@ export default function CustomTypical({
   return (
     <animated.span ref={ref} style={props} className={classes.spring}>
       {count && (
-        <Typist
+        <StyledTypist
           avgTypingDelay={90}
           stdTypingDelay={30}
           startDelay={1000}
@@ -37,12 +56,12 @@ export default function CustomTypical({
           className={classes.typical}
         >
           <span>{typicalOne}</span>
-          <Typist.Backspace count={typicalOne.length} delay={900} />
+          <Typist.Backspace count={typicalOne.length} delay={3000} />
           <span>{typicalTwo}</span>
-          <Typist.Backspace count={typicalTwo.length} delay={900} />
+          <Typist.Backspace count={typicalTwo.length} delay={3000} />
           <span>{typicalThree}</span>
-          <Typist.Backspace count={typicalThree.length} delay={3000} />
-        </Typist>
+          <Typist.Backspace count={typicalThree.length} delay={9000} />
+        </StyledTypist>
       )}
     </animated.span>
   );
